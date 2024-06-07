0f4623ec9d251214af98ec6d81b0e7e9fa046695
Justin8606 <justinshajuk@gmail.com>
2024-06-08 00:29:08 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import AddAlbum from './components/AddAlbum';
+import SearchAlbum from './components/SearchAlbum';
+import DeleteAlbum from './components/DeleteAlbum';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
-    <div>
-      <AddAlbum/>
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<AddAlbum/>}/>
+    <Route path='/Search' element={<SearchAlbum/>}/>
+    <Route path='/Delete' element={<DeleteAlbum/>}/>
+    <Route path='/View' element={<ViewAll/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 
-const AddPost = () => {
+const AddAlbum = () => {
   return (
     <div>
-        
+        <NavBar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -42,4 +43,4 @@ const AddPost = () => {
   )
 }
 
-export default AddPost
+export default AddAlbum
